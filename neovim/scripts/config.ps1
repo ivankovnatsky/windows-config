8fcce2cfd8c25b0750528ca5f989a484c721b07b
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-07-13 11:19:20 +0300

    neovim: remove consolidated directory

--- neovim/scripts/config.ps1
+++
@@ -1,7 +0,0 @@
-# UTF8 encoding for emojis
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-
-function Deploy-LazyVimConfig {
-    Write-Host "Ready for custom configurations..." -ForegroundColor Yellow
-    return $true
-}
--- neovim/scripts/utils.ps1
+++
@@ -1,8 +0,0 @@
-# Helper functions
-function Write-ColorMessage {
-    param(
-        [string]$Message,
-        [string]$Color = "White"
-    )
-    Write-Host $Message -ForegroundColor $Color
-} 
