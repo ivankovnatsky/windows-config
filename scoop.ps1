41bb7135b445f5ebc94e09ff4343a7f6e4566911
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-07-16 21:38:03 +0300

    scoop: enable aria2

--- scoop.ps1
+++ scoop.ps1
@@ -1,10 +1,10 @@
 $scoopConfig = @"
 {
-    "aria2-enabled": false,
+    "aria2-enabled": true,
     "aria2-warning-enabled": false,
-    "last_update": "2024-01-01T00:00:00.0000000+00:00",
-    "SCOOP_REPO": "https://github.com/ScoopInstaller/Scoop",
-    "SCOOP_BRANCH": "master"
+    "last_update":  "2025-07-16T20:45:11.7248964+03:00",
+    "scoop_repo":  "https://github.com/ScoopInstaller/Scoop",
+    "scoop_branch":  "master"
 }
 "@
 
