fde304e3e5e8cb4a50abf54fd6723a3d2508b724
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-08-03 16:01:48 +0300

    dotfiles/firefox: move to dotfiles

---
+++ dotfiles/AppData/Roaming/Mozilla/Firefox/Profiles/l07nai52.default-release-1/user.js
@@ -0,0 +1,9 @@
+// Enable vertical tabs
+user_pref("sidebar.verticalTabs", true);
+
+// Restore previous session (tabs and windows)
+user_pref("browser.startup.page", 3);
+
+// Enable Ctrl+Tab to cycle through recent tabs
+user_pref("browser.ctrlTab.recentlyUsedOrder", true);
+user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
